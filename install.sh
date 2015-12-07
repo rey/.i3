03b57538b2e1dbdd28209fa5e4ee0e1904fbad3a
Rey Dhuny <rey@users.noreply.github.com>
2015-12-07 14:23:04 +0000

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,2 +1,3 @@
-ln -s ~/.i3/.i3status.conf ~/.i3status.conf
-ln -s ~/.i3/.Xresources ~/.Xresources
+ln -s ~/computer/config/i3 ~/.config/i3/config
+ln -s ~/computer/config/i3status ~/.config/i3status/config
+ln -s ~/computer/config/terminal ~/.config/xfce4/Terminal/terminalrc
