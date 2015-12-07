8ac69ce225655562edd27f48c6b308428a44ee54
Rey Dhuny <rey@users.noreply.github.com>
2015-12-07 20:32:36 +0000

    Update install.sh

--- install.sh
+++ install.sh
@@ -3,24 +3,24 @@ echo
 mkdir -p ~/.config/i3
 mkdir ~/.config/i3status
 mkdir -p ~/.config/xfce4/terminal
-
+echo
 echo "CREATE SYMLINKS"
 echo
 ln -s ~/computer/config/i3 ~/.config/i3/config
 ln -s ~/computer/config/i3status ~/.config/i3status/config
 ln -s ~/computer/config/terminal ~/.config/xfce4/terminal/terminalrc
-
+echo
 echo "USE OFFICIAL i3 REPO"
 echo
 echo "deb http://debian.sur5r.net/i3/ $(lsb_release -c -s) universe" | sudo tee -a /etc/apt/sources.list
 sudo apt-get update
 sudo apt-get --allow-unauthenticated install sur5r-keyring
-
+echo
 echo "UPDATE & UPGRADE"
 echo
 sudo apt-get update
 sudo apt-get --assume-yes upgrade
-
+echo
 echo "INSTALL STUFF"
 echo
 sudo apt-get --assume-yes install i3 chromium-browser feh git saidar tmux tree vim xfce4-terminal xinit
