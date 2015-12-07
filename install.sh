dbba16bf150c0b30c6bfd95c8ca57ef8f9c6b842
Rey Dhuny <rey@me.com>
2015-12-07 15:38:19 +0000

    Update `install.sh` with fixes for using official i3 repo

--- install.sh
+++ install.sh
@@ -10,10 +10,16 @@ ln -s ~/computer/config/i3 ~/.config/i3/config
 ln -s ~/computer/config/i3status ~/.config/i3status/config
 ln -s ~/computer/config/terminal ~/.config/xfce4/Terminal/terminalrc
 
+echo "USE OFFICIAL i3 REPO"
+echo
+sudo echo "deb http://debian.sur5r.net/i3/ $(lsb_release -c -s) universe" | sudo tee -a /etc/apt/sources.list
+sudo apt-get update
+sudo apt-get --allow-unauthenticated install sur5r-keyring
+
 echo "UPDATE & UPGRADE"
 echo
 sudo apt-get update
 sudo apt-get --assume-yes upgrade
 
 echo "INSTALL STUFF"
-sudo apt-get --assume-yes install i3 chromium-browser git saidar tmux tree vim xfce4-terminal
+sudo apt-get --assume-yes install i3 chromium-browser git saidar tmux tree vim xfce4-terminal xinit
